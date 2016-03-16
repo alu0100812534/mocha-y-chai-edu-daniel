efb619ee8acaa8b5c064f506d0c25ca931f46cf1
alu0100825985 <alu0100825985@ull.edu.es>
2016-03-16 18:22:34 +0000

    Prueba superada con éxito

--- conversor_test.js
+++ conversor_test.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 describe("Medida", function() {
   	describe("constructor", function() {
     	it("Creación de una medida con dos argumentos", function() {
-      		var medida = new Medida(32, Celsius);
+      		var medida = new Medida(32, 'Celsius');
       		expect(medida.valor).to.equal(32);
       		expect(medida.tipo).to.equal("Celsius");
     	});
