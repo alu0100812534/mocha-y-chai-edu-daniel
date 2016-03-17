633c6b867e081aeff0fa787a5fb04022e8394cef
alu0100825985 <alu0100825985@ull.edu.es>
2016-03-17 20:19:56 +0000

    Creado con éxito el directorio minified con los ficheros minificados

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify');
 
 //-----> CONFIGURACIÓN DE LAS TAREAS
-gulp.task('demo', function () { // gulp.task: Definición de una tarea.
-gulp.src('js/*.js') // Indicamos los ficheros.
-.pipe(uglify('minified/'))
-.pipe(gulp.dest('js/build/'))
+gulp.task('uglify', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('minified/'))
 });
