20addbb120d5d45d5cc69d80dc57d4bd134d375d
alu0100825985 <alu0100825985@ull.edu.es>
2016-03-17 23:09:01 +0000

    Añadida la tarea clean al gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 
 //-----> DEPENDENCIAS
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	clean = require('gulp-clean');
 
 //-----> CONFIGURACIÓN DE LAS TAREAS
 gulp.task('uglify', function() {
   gulp.src('js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('minified/'))
+});
+
+gulp.task('clean', function () {
+	return gulp.src('minified/', {read: false})
+		.pipe(clean());
 });
