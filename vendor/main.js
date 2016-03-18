7548cf78a4086c65499f44c28db7b60f303b005e
alu0100825985 <alu0100825985@ull.edu.es>
2016-03-18 10:33:00 +0000

    Actualizado el gulpfile.js para minificar CSS y HTML

--- vendor/main.js
+++
@@ -1,1 +0,0 @@
-!function(e){"use strict";function n(){var e=document.getElementById("convert").value,n=document.getElementById("converted");return n.innerHTML=Medida.convertir(e),!1}e.main=n}(this);
